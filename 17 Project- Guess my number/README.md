6ef05d843d5b1cafc5b4ea3fe66838a484efd708
Chaikrit the coder <110481878+chaikrit030@users.noreply.github.com>
2022-08-28 11:59:21 +0530

    Add files via upload

---
+++ 17 Project- Guess my number/README.md
@@ -0,0 +1,13 @@
+<h1 align="center">
+🔢Guess the Number
+</h1>
+
+#### A old School game built using vanillaJs . Simple and fun game
+##### you can check it out from here [Game](https://guess-thenumbergame.netlify.app).
+
+**functionality**
+- restart the game 
+- restoring highscore
+- updating score 
+
+<img src="ss.jpg">
---
+++ 17 Project- Guess my number/app.js
@@ -0,0 +1,61 @@
+'use-strict';
+
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+//The DRY Principle follows here as again n again we are not callign the message simple using function for it
+const showMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const showNumber= function (para){
+    document.querySelector('.number').textContent= para;
+}
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    
+    showMessage('⛔️ No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    showMessage('🎉 Correct Number!');
+     showNumber(secretNumber);
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      showMessage(guess > secretNumber ? '⬆️ Too high!' : '⬇️ Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      showMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  showMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  showNumber ('?');
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
